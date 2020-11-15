441aca32c92c5e01e00c58aa048c41da601c797d
Leo Portemont <leo.portemont@portemont.eu>
2020-11-15 19:47:35 +0100

    backend: add models to django admin

--- back/academysite/academy/admin.py
+++ back/academysite/academy/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'track',
+        'problem_id',
+        'submission_date',
+        'passed',
+    )
+    search_fields = (
+        'problem_id',
+        'author__username',
+        'track__name',
+    )
+    list_filter = (
+        'problem_id',
+        'track',
+        'passed',
+    )
+
+@admin.register(models.TrackInstance)
+class TrackInstanceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'track_id',
+        'public',
+        'archived'
+    )
+    search_fields = (
+        'name',
+        'track_id',
+    )
+    list_filter = (
+        'track_id',
+        'public',
+        'archived',
+    )
