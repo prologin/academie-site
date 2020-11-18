4965095148f7601c788330d939e03f3f83df5e89
Leo Portemont <leo.portemont@portemont.eu>
2020-11-18 13:09:45 +0100

    back/api: reorganize file structure

--- back/academysite/academy/urls.py
+++ back/academysite/academy/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
+from . import views
 
 app_name = 'academy'
 
@@ -18,5 +21,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     re_path(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^doc/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', include('academy.api.routes')),
 ]
+
+router = routers.DefaultRouter()
+router.register('track', views.TrackInstanceViewSet, basename='track')
+router.register('submission', views.SubmissionViewSet, basename='submission')
+
+track_subrouter = nested_routers.NestedSimpleRouter(router, 'track', lookup='track')
+track_subrouter.register('problem', views.ProblemViewSet, basename='track-problems')
+
+urlpatterns += router.urls
+urlpatterns += track_subrouter.urls
