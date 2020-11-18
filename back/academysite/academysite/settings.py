9f81609a078987397aa2378f97edaa23d966cf1f
Leo Portemont <leo.portemont@portemont.eu>
2020-11-18 02:14:31 +0100

    back/cors: add corsheaders

--- back/academysite/academysite/settings.py
+++ back/academysite/academysite/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'academy',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = DEBUG
 
 # Required : ACADEMY_RESOURCES_PATH
 ACADEMY_RESOURCES_PATH = '/home/leo/prologin/academy-tracks'
