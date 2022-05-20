c5d0bc542032bb226749c569f8adee5b8bdc1000
Adrien LANGOU <adrien.langou@epita.fr>
2022-05-20 12:10:52 +0200

    back: add endpoints and update celery config

--- evaluator/evaluator/evaluator/celery.py
+++ evaluator/evaluator/evaluator/celery.py
@@ -9,3 +9,9 @@ app = Celery("evaluator")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+
+app.conf.broker_url = BASE_REDIS_URL
+
+app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
--- evaluator/evaluator/evaluator/urls.py
+++ evaluator/evaluator/evaluator/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from misc.views import SchemaView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/activities/", include("activities.urls")),
+    path("api/problems/", include("problems.urls")),
+    path("api/status/", include("status.urls")),
+    path("auth/", include("authentification.urls")),
+    path("swagger/", SchemaView.with_ui("swagger", cache_timeout=0), name="swagger",),
+    path("redoc/", SchemaView.with_ui("redoc", cache_timeout=0), name="swagger",),
 ]
