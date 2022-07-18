09a7e890d85d5012fee3b0bd37c7401007c9f7f8
Adrien Langou <adrien.langou@prologin.org>
2022-07-18 11:12:02 +0200

    back, front: rework architecture to match django-template architecture

--- front/src/api/userApi.js
+++
@@ -1,9 +0,0 @@
-import { httpClient } from './httpClient';
-
-import { USER_API_URL } from './constants';
-
-const getMyProfile = async () => httpClient.get(`${USER_API_URL}me/`);
-
-const userApi = { getMyProfile };
-
-export default userApi;
