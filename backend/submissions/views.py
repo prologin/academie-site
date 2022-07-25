a145c3c1d115ab92492c06b6e91173996b25e523
Adrien Langou <adrien.langou@prologin.org>
2022-07-25 11:32:08 +0200

    back: update submission view so that a submission must pass through an activity

--- backend/submissions/views.py
+++ backend/submissions/views.py
@@ -1,13 +1,13 @@
-from cgitb import lookup
+from activities.models import Activity
+from problems.models import Problem
 
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, status, viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.response import Response
+from rest_framework import status
 
-from problems.models import Problem
 from status.models import Status
 from status.serializers import StatusSerializer
 from submissions.models import ProblemSubmission, ProblemSubmissionCode
@@ -16,6 +16,14 @@ from submissions.tasks import run_code_submission
 
 User = get_user_model()
 
+'''
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+'''
 
 class SubmissionView(
     mixins.CreateModelMixin,
@@ -24,26 +32,28 @@ class SubmissionView(
 ):
     serializer_class = ProblemSubmissionCodeSerializer
     queryset = ProblemSubmissionCode.objects.all()
-    lookup_field = "id"
+#    lookup_field = "id"
 
-    def retrieve(self, request, id=None):  # get with parameter
-        return super().retrieve(request, id)
+#    def retrieve(self, request, id=None):  # get with parameter
 
-    def create(self, request):
+
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+        serializer.is_valid(raise_exception=True)
 
-        problem = get_object_or_404(Problem, title=request.data["title"])
-
+        activity = get_object_or_404(Activity, id=serializer.validated_data['activity_id'])
+        if not activity.problems.all().filter(id__in=[serializer.validated_data['problem_id']]).exists():
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"Detail": "The problem is not part of the activity"})
+        problem = Problem.objects.get(id=serializer.validated_data['problem_id'])
+        if not serializer.validated_data['language'] in problem.allowed_languages:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"Detail": "This language is not available for the problem"})
+        
         problem_submission, _ = ProblemSubmission.objects.get_or_create(
             problem=problem,
-            user=request.user,
+            user=self.request.user,
             defaults={
                 "problem": problem,
-                "user": request.user,
+                "user": self.request.user,
             },
         )
 
@@ -51,15 +61,15 @@ class SubmissionView(
 
         try:
             correction_template += problem.correction_templates[
-                request.data["language"]
+                serializer.validated_data["language"]
             ]
         except:
             correction_template = ""
 
         problem_submission_code = ProblemSubmissionCode.objects.create(
             submission=problem_submission,
-            language=request.data["language"],
-            code=request.data["code"] + "\n\n\n\n\n" + correction_template,
+            language=serializer.validated_data["language"],
+            code=serializer.validated_data["code"] + "\n\n\n\n\n" + correction_template,
             summary="Aurel c'est trop mon binome",
         )
 
@@ -72,7 +82,6 @@ class SubmissionView(
         )
 
         serializer = self.get_serializer(task_model)
+
         headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
